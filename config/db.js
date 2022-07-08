920d58c861637f5df923f7184d192d0d066e3b5e
Lucas Gabriel <lucasgabrielba@gmail.com>
2022-07-08 12:03:26 -0300

    correção bd

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV == "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://lucasalvesba:mongo123@cluster0.va94pd4.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://lucasalvesba:mongo123@cluster0.j7ugs6s.mongodb.net/?retryWrites=true&w=majority",
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/blogapp" };
