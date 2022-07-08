bf11dc27e0755f75808ad7ef24b45187dba9ff40
Lucas Gabriel <lucasgabrielba@gmail.com>
2022-07-08 11:06:12 -0300

    alteração no bd

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV == "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://lucasalvesba:mongoloide@cluster0.va94pd4.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://lucasalvesba:mongo123@cluster0.va94pd4.mongodb.net/?retryWrites=true&w=majority",
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/blogapp" };
